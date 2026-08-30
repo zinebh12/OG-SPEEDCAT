b116279d2e95b29d2dc48dc6c059f5d083259304
Zineb <96913718+wx-it@users.noreply.github.com>
2026-08-30 12:05:27 +0100

    done with main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { motion } from "motion/react";
+import { AnimatePresence } from "motion/react";
 import redSpeedcat from "./assets/red-speedcat.png";
 import graySpeedcat from "./assets/gray-speedcat.png";
 import navySpeedcat from "./assets/navy-speedcat.png";
 import greenSpeedcat from "./assets/green-speedcat.png";
 import puma from "./assets/puma.png";
 import search from "./assets/search.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBarsStaggered, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 function App() {
   const [currentShoe, setCurrentShoe] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const shoes = [
     {
       image: redSpeedcat,
@@ -44,6 +48,94 @@ function App() {
     >
       <nav className="flex justify-between items-center p-4 w-full">
         <img className="max-h-[5vh]" src={puma} alt="" />
+        <button className="md:hidden">
+          <FontAwesomeIcon
+            icon={faBarsStaggered}
+            className="text-white text-2xl"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          />
+        </button>
+        <AnimatePresence>
+          {isMenuOpen && (
+            <motion.div
+              initial={{ opacity: 0, x: 0 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 0 }}
+              transition={{ duration: 0.4, ease: "easeInOut" }}
+              className=" z-50 md:hidden fixed inset-0 h-screen bg-black/50 backdrop-blur-md flex flex-col justify-center items-center"
+            >
+              <button
+                className="md:hidden absolute top-6 right-4"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                <FontAwesomeIcon
+                  icon={faXmark}
+                  className="text-white text-2xl"
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                />
+              </button>
+              <ul
+                className="space-y-10 text-4xl"
+                // className="md:hidden gap-7 p-4 px-10 absolute inset-0 h-screen bg-black/50 backdrop-blur-md flex flex-col justify-center items-center"
+              >
+                <motion.li
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 0.4, ease: "easeOut" }}
+                  className="cursor-pointer"
+                >
+                  Home
+                </motion.li>
+                <motion.li
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 0.6, ease: "easeOut" }}
+                  className="cursor-pointer"
+                >
+                  Shop
+                </motion.li>
+                <motion.li
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 0.8, ease: "easeOut" }}
+                  className="cursor-pointer"
+                >
+                  Collection
+                </motion.li>
+                <motion.li
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 1, ease: "easeOut" }}
+                  className="cursor-pointer"
+                >
+                  New Releases
+                </motion.li>
+                <motion.li
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 1.2, ease: "easeOut" }}
+                  className="cursor-pointer"
+                >
+                  Blog
+                </motion.li>
+                <motion.li
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 1.4, ease: "easeOut" }}
+                  className="cursor-pointer"
+                >
+                  About Us
+                </motion.li>
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
         <ul className="hidden md:flex gap-7 p-4 px-10  rounded-full">
           <li className="cursor-pointer">Home</li>
           <li className="cursor-pointer">Shop</li>
@@ -52,11 +144,7 @@ function App() {
           <li className="cursor-pointer">Blog</li>
           <li className="cursor-pointer">About Us</li>
         </ul>
-        <img
-          className="hidden md:block max-h-[4vh] mb-2"
-          src={search}
-          alt=""
-        />
+        <img className="hidden md:block max-h-[4vh] mb-2" src={search} alt="" />
       </nav>
 
       <div className="h-full w-full flex flex-col justify-between items-end ">
@@ -95,17 +183,13 @@ function App() {
             />
           </motion.div>
         </div>
-        <div className="font-barlow z-10 md:absolute md:right-0 md:bottom-5 space-y-1 p-4 h-fit">
+        <div className=" font-barlow z-10 md:absolute md:right-0 md:bottom-5 space-y-1 p-4 h-fit">
           <h2 className="font-barlow font-bold text-sm md:text-xl">Speedcat</h2>
           <p className="text-xs md:text-sm tracking-widest max-w-[70vh]">
             The Puma Speedcat is a classic sneaker originally inspired by
             motorsports, particularly Formula 1 racing, and designed for both
             performance and style.
           </p>
-          {/* <button className="bg-peachy py-2 px-6 rounded-full flex items-center justify-between gap-1">
-            <span>Get Started </span>
-            <img className="max-h-[3vh]" src={arrow} alt="" />
-          </button> */}
         </div>
       </div>
     </motion.div>
